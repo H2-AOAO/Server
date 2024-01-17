17949149803f6d66cfd1883eaaedac025c54cde9
yukong <kyb062666@gmail.com>
2024-01-17 16:58:12 +0900

    [REFACTOR] 생성자 어노테이션 수정

--- src/main/java/kr/sesac/aoao/server/controller/dto/response/ExceptionResponse.java
+++ src/main/java/kr/sesac/aoao/server/controller/dto/response/ExceptionResponse.java
@@ -1,10 +1,10 @@
 package kr.sesac.aoao.server.controller.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ExceptionResponse {
 
     private final String message;
