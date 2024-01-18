d6e2475846371d4b342942b7620a2c070b74192e
sangminee <lsmlee99@gmail.com>
2024-01-18 15:26:39 +0900

    [feat] BaseEntity, UserRepository 추가

---
+++ src/main/java/kr/sesac/aoao/server/user/infrastructure/Role.java
@@ -0,0 +1,5 @@
+package kr.sesac.aoao.server.user.infrastructure;
+
+public enum Role {
+    USER, MANAGER, ADMIN;
+}
---
+++ src/main/java/kr/sesac/aoao/server/user/infrastructure/UserEntityRepository.java
@@ -0,0 +1,11 @@
+package kr.sesac.aoao.server.user.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+}
