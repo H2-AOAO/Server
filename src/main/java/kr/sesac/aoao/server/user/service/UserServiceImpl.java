21fae019c7ae20c204cc5c9aa0a19876c9fb2f60
sangminee <lsmlee99@gmail.com>
2024-01-19 16:48:42 +0900

    [FIX] 리뷰 반영, @build 제거

--- src/main/java/kr/sesac/aoao/server/user/service/UserServiceImpl.java
+++ src/main/java/kr/sesac/aoao/server/user/service/UserServiceImpl.java
@@ -20,7 +20,7 @@ import lombok.RequiredArgsConstructor;
  * @since 2024.01.18
  */
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 @Service
 public class UserServiceImpl implements UserService {
 
