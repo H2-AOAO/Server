5e8eeceafc209ba7250b933f476b4004568835ed
sangminee <lsmlee99@gmail.com>
2024-01-19 11:14:55 +0900

    [CHORE] main과 merge

--- src/main/java/kr/sesac/aoao/server/user/controller/dto/response/JwtTokenResponse.java
+++ src/main/java/kr/sesac/aoao/server/user/controller/dto/response/JwtTokenResponse.java
@@ -1,8 +1,10 @@
 package kr.sesac.aoao.server.user.controller.dto.response;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class JwtTokenResponse {
 
 	private String accessToken;
