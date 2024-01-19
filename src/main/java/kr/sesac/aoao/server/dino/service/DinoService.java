494a30e84ee148f6bd473cfb7622662514ab154d
kimeunseo58 <alan0221@naver.com>
2024-01-19 19:01:24 +0900

    [CHORE] DinoJpaRepository Optional & DTO 수정

--- src/main/java/kr/sesac/aoao/server/dino/service/DinoService.java
+++ src/main/java/kr/sesac/aoao/server/dino/service/DinoService.java
@@ -1,9 +1,7 @@
 package kr.sesac.aoao.server.dino.service;
 
-import java.util.Optional;
-
 import kr.sesac.aoao.server.dino.controller.dto.GetUserDinoResponse;
 
 public interface DinoService {
-	Optional<GetUserDinoResponse> getDinoInfo(Long userId);
+	GetUserDinoResponse getDinoInfo(Long userId);
 }
