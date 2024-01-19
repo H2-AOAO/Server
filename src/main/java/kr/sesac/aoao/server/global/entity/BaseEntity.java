28cdc12f5d97772d4b240e886ae75c2a3d7dc3bd
yukong <kyb062666@gmail.com>
2024-01-19 16:11:19 +0900

    [REFACTOR] main 충돌 해결

--- src/main/java/kr/sesac/aoao/server/global/entity/BaseEntity.java
+++ src/main/java/kr/sesac/aoao/server/global/entity/BaseEntity.java
@@ -7,6 +7,9 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
@@ -14,6 +17,10 @@ import lombok.Getter;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	@CreatedDate
 	private LocalDateTime createdAt;
