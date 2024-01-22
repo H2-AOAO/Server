6a03a672f3c898307ee2878e1b23dc134bb1dfa6
kimeunseo58 <alan0221@naver.com>
2024-01-22 15:49:29 +0900

    [CHORE] 에러코드 추가

--- src/main/java/kr/sesac/aoao/server/item/exception/ItemErrorCode.java
+++ src/main/java/kr/sesac/aoao/server/item/exception/ItemErrorCode.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ItemErrorCode implements ErrorCode {
 	NOT_FOUND_ITEM(HttpStatus.BAD_REQUEST, "존재하지 않는 아이템입니다."),
-	NOT_FOUND_USER_ITEM(HttpStatus.BAD_REQUEST, "사용자의 아이템 정보를 찾을 수 없습니다.");
+	NOT_FOUND_USER_ITEM(HttpStatus.BAD_REQUEST, "사용자의 아이템 정보를 찾을 수 없습니다."),
+	NOT_ENOUGH_ITEM(HttpStatus.BAD_REQUEST, "아이템의 개수가 모자랍니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
