6be39e902fb401d4eca07860352d4ee613eefaff
kimeunseo58 <alan0221@naver.com>
2024-01-22 15:46:24 +0900

    [CHORE] 테이블 구조 변경 후 아이템 개수 변경 API 수정

--- src/test/java/kr/sesac/aoao/server/dino/service/DinoServiceImplTest.java
+++ src/test/java/kr/sesac/aoao/server/dino/service/DinoServiceImplTest.java
@@ -1,59 +1,71 @@
 package kr.sesac.aoao.server.dino.service;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import kr.sesac.aoao.server.dino.controller.dto.GetUserDinoResponse;
+import kr.sesac.aoao.server.dino.domain.Dino;
+import kr.sesac.aoao.server.dino.repository.DinoEntity;
+import kr.sesac.aoao.server.dino.repository.DinoInfoEntity;
 import kr.sesac.aoao.server.dino.repository.DinoJpaRepository;
-import kr.sesac.aoao.server.item.repository.ItemJpaRepository;
+import kr.sesac.aoao.server.user.repository.UserEntity;
 import kr.sesac.aoao.server.user.repository.UserJpaRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class DinoServiceImplTest {
-	private DinoService dinoService;
-
-	@Mock
-	private DinoJpaRepository dinoRepository;
-
-	@Mock
-	private UserJpaRepository userRepository;
-
-	@Mock
-	private ItemJpaRepository itemRepository;
-
-	@BeforeEach
-	void setUp() {
-		dinoService = new DinoServiceImpl(
-			dinoRepository, userRepository, itemRepository
-		);
-	}
-
+	// private DinoService dinoService;
+	//
+	// @Mock
+	// private DinoJpaRepository dinoRepository;
+	//
+	// @Mock
+	// private UserJpaRepository userRepository;
+	//
+	// @BeforeEach
+	// void setUp(){
+	// 	dinoService = new DinoServiceImpl(
+	// 		dinoRepository,userRepository
+	// 	);
+	// }
+	//
 	// @DisplayName("다이노 조회 테스트")
 	// @Nested
 	// class getDinoInfo{
-	//
 	// 	@DisplayName("정보 조회 성공")
 	// 	@Test
 	// 	void success(){
 	// 		long userId = 1L;
 	// 		long dinoId = 2L;
-	// 		UserEntity userEntity = mock(UserEntity.class);
+	// 		UserEntity user = mock(UserEntity.class);
 	//
-	// 		// mock
-	// 		User user = new User(userId, "email", "nickname", "password", "password2", "profile", null);
+	// 		//mock
+	// 		DinoEntity dino = mock(DinoEntity.class);
 	// 		DinoInfoEntity dinoInfo = mock(DinoInfoEntity.class);
-	// 		UserEntity user_ = new UserEntity(user);
+	// 		UserEntity user_ = mock(UserEntity.class);//(userId, "nickname", "email", "password", "profile", null, dino);
 	// 		when(userRepository.findById(userId))
 	// 			.thenReturn(Optional.of(user_));
-	// 		when(dinoRepository.findByUser(user_))
-	// 			.thenReturn(Optional.of(new DinoEntity(dinoId, userEntity, "name", "green",234,30,dinoInfo)));
+	// 		when(dinoRepository.findByUserId(userId))
+	// 			.thenReturn(Optional.of(new DinoEntity(dinoId, user, "name", "green",234,30,dinoInfo)));
 	//
 	// 		// when
 	// 		GetUserDinoResponse res = dinoService.getDinoInfo(userId);
 	//
 	// 		// then
-	// 		assertThat(res).isNotNull();
+	// 		assertThat(dinoId).isNotNull();
+	//
+	//
+	//
 	// 	}
 	//
 	// 	@DisplayName("유저 조회 시 오류 발생")
@@ -77,9 +89,10 @@ public class DinoServiceImplTest {
 	// 		long userId = 1L;
 	//
 	// 		//mock
-	// 		User user = new User (userId, "email", "nickname", "password", "password2", "profile", null);
+	// 		DinoEntity dino = mock(DinoEntity.class);
+	// 		UserEntity user__ = mock(UserEntity.class);
 	// 		when(userRepository.findById(userId))
-	// 			.thenReturn(Optional.of(new UserEntity(user)));
+	// 			.thenReturn(Optional.of(user__));
 	// 		when(dinoRepository.findById(notExistDinoId))
 	// 			.thenReturn(Optional.empty());
 	//
