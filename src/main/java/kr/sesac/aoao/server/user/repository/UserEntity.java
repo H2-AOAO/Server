2ddaab6a03deda38c051f0e61ff584abd08cff9f
sangminee <lsmlee99@gmail.com>
2024-01-23 12:04:26 +0900

    [CHORE] main과 merge하기

--- src/main/java/kr/sesac/aoao/server/user/repository/UserEntity.java
+++ src/main/java/kr/sesac/aoao/server/user/repository/UserEntity.java
@@ -1,5 +1,7 @@
 package kr.sesac.aoao.server.user.repository;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
