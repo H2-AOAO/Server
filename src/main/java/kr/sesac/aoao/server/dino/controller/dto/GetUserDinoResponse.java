79517b2b7f28b7f053894711ed8dbc5a831ba5d0
kimeunseo58 <alan0221@naver.com>
2024-01-23 19:49:15 +0900

    [CHORE] 생성자 삭제 후 어노테이션 추가

--- src/main/java/kr/sesac/aoao/server/dino/controller/dto/GetUserDinoResponse.java
+++ src/main/java/kr/sesac/aoao/server/dino/controller/dto/GetUserDinoResponse.java
@@ -1,8 +1,10 @@
 package kr.sesac.aoao.server.dino.controller.dto;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class GetUserDinoResponse {
 
 	private final Long userId;
@@ -12,12 +14,4 @@ public class GetUserDinoResponse {
 	private final int Lv;
 	private final Integer point;
 
-	public GetUserDinoResponse(Long userId, String name, String color, Integer exp, int lv, Integer point) {
-		this.userId = userId;
-		this.name = name;
-		this.color = color;
-		this.exp = exp;
-		this.Lv = lv;
-		this.point = point;
-	}
 }
--- src/main/java/kr/sesac/aoao/server/item/controller/dto/GetItemInfoResponse.java
+++ src/main/java/kr/sesac/aoao/server/item/controller/dto/GetItemInfoResponse.java
@@ -1,19 +1,16 @@
 package kr.sesac.aoao.server.item.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class GetItemInfoResponse {
+
 	private final Long id;
 	private final String name;
 	private final int price;
 	private final int exp;
-
-	public GetItemInfoResponse(Long id, String name, int price, int exp){
-		this.id = id;
-		this.name = name;
-		this.price = price;
-		this.exp = exp;
-	}
 }
