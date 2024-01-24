8429407d3fa97860213a81e127160e11fd5d769a
kyukong <92148749+kyukong@users.noreply.github.com>
2024-01-24 17:15:15 +0900

    [FEAT] 포인트 관련 엔티티 생성 (#39)

---
+++ src/main/java/kr/sesac/aoao/server/point/repository/PointEntity.java
@@ -0,0 +1,33 @@
+package kr.sesac.aoao.server.point.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import kr.sesac.aoao.server.global.entity.BaseEntity;
+import kr.sesac.aoao.server.user.repository.UserEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "point")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PointEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private int point = 0;
+
+    @OneToOne(mappedBy = "point")
+    private UserEntity user;
+}
---
+++ src/main/java/kr/sesac/aoao/server/point/repository/PointJpaRepository.java
@@ -0,0 +1,6 @@
+package kr.sesac.aoao.server.point.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointJpaRepository extends JpaRepository<PointEntity, Long> {
+}
