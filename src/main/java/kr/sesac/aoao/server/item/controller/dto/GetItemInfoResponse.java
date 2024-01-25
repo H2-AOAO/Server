8cc4223d32ece27be6c429d1508bddf1b4bf9918
jeong-yooon <102950984+jeong-yooon@users.noreply.github.com>
2024-01-25 16:26:01 +0900

    [CHORE] 다이어리 API 통신 테스트 완료 코드 (#48)
    * [CHORE] 다이어리 API 통신 테스트 완료 코드
    
    * [CHORE] API 통신 테스트 완료 코드

---
+++ src/main/java/kr/sesac/aoao/server/item/controller/dto/GetItemInfoResponse.java
@@ -0,0 +1,14 @@
+package kr.sesac.aoao.server.item.controller.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class GetItemInfoResponse {
+
+	private final Long id;
+	private final String name;
+	private final int price;
+	private final int exp;
+}
