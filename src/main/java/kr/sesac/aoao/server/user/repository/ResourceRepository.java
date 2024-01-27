7e78435ca1feb21d728bac6e40b882fc974ac71e
sangminee <lsmlee99@gmail.com>
2024-01-28 00:14:48 +0900

    [FIX] entity 명 변경

--- src/main/java/kr/sesac/aoao/server/user/repository/ResourceRepository.java
+++
@@ -1,6 +0,0 @@
-package kr.sesac.aoao.server.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResourceRepository extends JpaRepository<Resource, Long> {
-}
