f22199037ea4fa68bb8cc8aeebc9d9ee435f3f11
kyukong <92148749+kyukong@users.noreply.github.com>
2024-01-28 04:30:24 +0900

    [REFACTOR] 투두리스트 폴더 색상 추가 (#78)

--- src/main/java/kr/sesac/aoao/server/todo/domain/Palette.java
+++ src/main/java/kr/sesac/aoao/server/todo/domain/Palette.java
@@ -5,14 +5,16 @@ import lombok.Getter;
 @Getter
 public enum Palette {
 
-	YELLOW("#F3F705"),
-	ORANGE("#FA602F"),
-	GREEN("#8EF705"),
-	PURPLE("#B92FFA"),
-	HOT_PINK("#FA2F7D"),
-	MINT("#31F5D7"),
-	SKY_BLUE("#31D2F5"),
-	BLUE("#3194F5"),
+	YELLOW("yellow"),
+	ORANGE("orange"),
+	GREEN("green"),
+	SKY_BLUE("sky_blue"),
+	PINK("pink"),
+	RED("red"),
+	PURPLE("purple"),
+	BLUE("blue"),
+	LIGHT_GREEN("light_green"),
+	MINT("mint"),
 	;
 
 	Palette(String colorCode) {
