803d9e12a2ad0c28b56110b4dcc8a66d1d8c545a
kyukong <92148749+kyukong@users.noreply.github.com>
2024-01-27 14:48:50 +0900

    [FEATURE] 마이페이지 API 구현 (#68)
    * [REFACTOR] 사용자 검증 컨트롤러 분리
    
    * [REFACTOR] 사용자 URI 수정
    
    * [FEAT] 사용자 닉네임 수정 기능 구현
    
    * [FEAT] 사용자 비밀번호 수정 기능 구현
    
    * [CHORE] s3 의존성 추가
    
    * [FEAT] s3 연결 객체 생성
    
    * [CHORE] s3 설정 파일 추가
    
    * [FEAT] User 에 Profile 객체 추가
    
    * [FEAT] 사용자 프로필 수정 기능 구현
    
    * [FEAT] 사용자 프로필 초기화 기능 구현
    
    * [FEATURE] dev 환경 배포 (#66)
    
    * [REFACTOR] entity 에 있는 oracle 예약어 변경
    
    * [FIX] h2-console 접근 제거
    
    * [CHORE] oracle 설정 및 profile 분리
    
    * [DOCS] 배포 스크립트 작성

---
+++ src/main/java/kr/sesac/aoao/server/user/repository/ResourceRepository.java
@@ -0,0 +1,6 @@
+package kr.sesac.aoao.server.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
+}
