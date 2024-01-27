97b7b183ca18a12ebbd90a6b69b5d656d3978d43
kimeunseo58 <alan0221@naver.com>
2024-01-27 21:57:28 +0900

    [CHORE] 유저 아이템 생성 코드 수정

--- src/main/java/kr/sesac/aoao/server/user/service/UserServiceImpl.java
+++ src/main/java/kr/sesac/aoao/server/user/service/UserServiceImpl.java
@@ -84,11 +84,16 @@ public class UserServiceImpl implements UserService {
 		 * @since 2024.01.26
 		 */
 		List<UserItemEntity> userItems = new ArrayList<>();
-		for (int i = 1; i <= 5; i++) {
-			ItemEntity itemEntity = itemJpaRepository.findById((long) i).orElse(null);
-			UserItemEntity userItem = new UserItemEntity(userEntity, itemEntity, 0);
+		List<ItemEntity> itemEntityList = itemJpaRepository.findAll();
+		for (ItemEntity item : itemEntityList) {
+			UserItemEntity userItem = new UserItemEntity(userEntity, item, 0);
 			userItems.add(userItem);
 		}
+		// for (int i = 1; i <= 5; i++) {
+		// 	//ItemEntity itemEntity = itemJpaRepository.findById((long) i).orElse(null);
+		// 	UserItemEntity userItem = new UserItemEntity(userEntity, itemEntityList.get(i), 0);
+		// 	userItems.add(userItem);
+		// }
 		userEntity.saveUserItems(userItems);
 		userJpaRepository.save(userEntity);
 
