bc70720d3a3f59e8719c19d92245546a45e191bf
yukong <kyb062666@gmail.com>
2024-01-28 17:38:17 +0900

    [FIX] 투두 폴더 생성 시 폴더 아이디 반환

---
+++ src/main/java/kr/sesac/aoao/server/todo/controller/dto/response/FolderSaveResponse.java
@@ -0,0 +1,11 @@
+package kr.sesac.aoao.server.todo.controller.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class FolderSaveResponse {
+
+    private final long folderId;
+}
